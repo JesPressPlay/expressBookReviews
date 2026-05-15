730fdd646a5459e76443a3a171e55df9e501aa2f
Jessie D'Andrea <jessiedandrea@Jessies-MacBook-Pro.local>
2026-05-15 13:26:26 -0400

    task 6 done

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,17 +7,39 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
   let userExists = users.find((user) => user.username === username);
-  return !userExists;
+  return !Boolean(userExists);
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+//Checking if username and password match the one we have in records.
+  let checkAuthenticaton = users.find((user) => user.username === username && user.password === password);
+  return Boolean(checkAuthenticaton);
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // First gotta check if username or password are missing
+  if (!username || !password) {
+    return res.status(404).json({message: "Error logging in, please try again."});
+  }
+
+  // Now, authenticate the user
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign({
+      data: username
+    }, 'access', { expiresIn: 3600 });
+
+    // Store access token and username in session
+    req.session.authorization = {
+      accessToken, username
+    }
+    return res.status(200).send("User successfully logged in!");
+  } else {
+    return res.status(208).json({ message: "Invalid login. Check your username and password. "});
+  }
 });
 
 // Add a book review
